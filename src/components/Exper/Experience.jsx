704ce6a46694acc5e1187441ec84498da6d973ec
Pablo Perez <paperezpablo@gmail.com>
2024-10-25 01:44:25 -0300

    Experience

--- src/components/Exper/Experience.jsx
+++ src/components/Exper/Experience.jsx
@@ -1,11 +1,16 @@
+import {EXPERIENCES} from '/src/constants'
 
 const Experience = () => {
   return (
     <div className="border-b border-neutral-800 pb-4">
-    <h2 className="my-20">Experience</h2>
-    
+      <h2 className="my-20 text-center text-4xl">Experience</h2>
+      <div>
+        {EXPERIENCES.map((Experience, index) => {
+          <div key={index} className="mb-8 flex-wrap lg:justify-center"></div>;
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
